4fca5b5afb4f60e7a8cc4ea67409c284e8488b59
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-09-18 09:32:30 +0200

    Finish and add Comment

--- WinFormSynchrone/Form1.Designer.cs
+++ WinFormSynchrone/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormSynchrone
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tlpMain = new TableLayoutPanel();
+            lbNumber = new ListBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            lbTime = new Label();
+            btSynchro = new Button();
+            tlpMain.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tlpMain
+            // 
+            tlpMain.ColumnCount = 1;
+            tlpMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tlpMain.Controls.Add(lbNumber, 0, 0);
+            tlpMain.Controls.Add(tableLayoutPanel1, 0, 1);
+            tlpMain.Dock = DockStyle.Fill;
+            tlpMain.Location = new Point(0, 0);
+            tlpMain.Name = "tlpMain";
+            tlpMain.RowCount = 2;
+            tlpMain.RowStyles.Add(new RowStyle(SizeType.Percent, 75.3333359F));
+            tlpMain.RowStyles.Add(new RowStyle(SizeType.Percent, 24.666666F));
+            tlpMain.Size = new Size(800, 450);
+            tlpMain.TabIndex = 0;
+            // 
+            // lbNumber
+            // 
+            lbNumber.Dock = DockStyle.Fill;
+            lbNumber.FormattingEnabled = true;
+            lbNumber.ItemHeight = 15;
+            lbNumber.Location = new Point(3, 3);
+            lbNumber.Name = "lbNumber";
+            lbNumber.Size = new Size(794, 333);
+            lbNumber.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 77.8337555F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 22.1662464F));
+            tableLayoutPanel1.Controls.Add(lbTime, 1, 0);
+            tableLayoutPanel1.Controls.Add(btSynchro, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(3, 342);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(794, 105);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // lbTime
+            // 
+            lbTime.Anchor = AnchorStyles.None;
+            lbTime.AutoSize = true;
+            lbTime.Location = new Point(663, 45);
+            lbTime.Name = "lbTime";
+            lbTime.Size = new Size(85, 15);
+            lbTime.TabIndex = 0;
+            lbTime.Text = "Temps écoulé :";
+            // 
+            // btSynchro
+            // 
+            btSynchro.Anchor = AnchorStyles.None;
+            btSynchro.Location = new Point(242, 25);
+            btSynchro.Name = "btSynchro";
+            btSynchro.Size = new Size(134, 55);
+            btSynchro.TabIndex = 1;
+            btSynchro.Text = "Synchrone";
+            btSynchro.UseVisualStyleBackColor = true;
+            btSynchro.Click += btSynchro_Click;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tlpMain);
+            Name = "frmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Task";
+            tlpMain.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tlpMain;
+        private ListBox lbNumber;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label lbTime;
+        private Button btSynchro;
     }
 }
--- WinFormSynchrone/Form1.cs
+++ WinFormSynchrone/Form1.cs
@@ -1,10 +1,57 @@
+using System.Diagnostics;
+
 namespace WinFormSynchrone
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        private List<int> _listNb = new List<int>();
+        Random random = new Random();
+        Stopwatch sw = new Stopwatch();
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void btSynchro_Click(object sender, EventArgs e)
+        {
+            //Vide la liste
+            _listNb.Clear();
+            //Reinitialise le data source
+            lbNumber.DataSource = null;
+            //Reinitialise le label qui indique le temps
+            lbTime.Text = string.Empty;
+            //Redemare le compteur de temps
+            sw.Restart();
+            //appel la fonction FillList
+            FillList();
+            //Stop le compteur de temps
+            sw.Stop();
+            //Inclue le temps passé dans la label
+            lbTime.Text = string.Format("Temps écoulé : {0} ms", sw.ElapsedMilliseconds);
+            //Classe la liste
+            _listNb.Sort();
+            //Ajoute la liste comme datasource de la list box
+            lbNumber.DataSource= _listNb;
+
+        }
+        private void FillList()
+        {
+            //Tant que la liste ne contient 100 nombre
+            while (_listNb.Count < 100)
+            {
+                //Tire un chiffre au hasard entre 1 et 100
+                int nb = random.Next(1, 101);
+                //Fait une pause de 10 ms à chaque nombre tiré
+                Thread.Sleep(10);
+                //Si la liste ne contient pas le nombre tiré
+                if (!_listNb.Contains(nb))
+                {
+                    //Ajoute le nombre à la liste
+                    _listNb.Add(nb);
+                    //Fait une pause de 10 ms à chaque nombre ajouté
+                    Thread.Sleep(10);
+                }
+            }
         }
     }
 }
